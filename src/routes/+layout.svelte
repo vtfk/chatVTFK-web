6c648fc7e46a0a2843f5620ed61c50e7e3cccf39
Robin Ellingsen <robin-elli@hotmail.com>
2023-04-18 14:46:47 +0200

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,22 @@
 <script>
+    import '../app.css'
     import { goto } from '$app/navigation'
     import { navigating } from "$app/stores"
     import { browser } from "$app/environment"
     import { web } from '../../config';
 	import { userStore } from '$lib/services/store';
 	import { get } from 'svelte/store';
-
+	import IconSpinner from '../components/Icons/IconSpinner.svelte';
+	import SideNav from '../components/SideNav.svelte';
+    
 
     let msal
     
     const user = get(userStore)
     console.log('Jeg kom hit i vercel')
-    if(browser && window.location.pathname === '/') {
-        window.location.href = `${web.url}/authenticated/chatVTFK`
-    }
+    // if(browser && window.location.pathname === '/') {
+    //     window.location.href = `${web.url}/authenticated/chatVTFK`
+    // }
     // If user is navigating, check if user have a valid token. If the token is not valid, do something
     $: if($navigating) {
         try {
@@ -42,4 +45,55 @@
     console.log('Jeg kom hit i vercel2')
 </script>
 
-<slot></slot>
+<div>
+    {#await msal}
+        <div class="centerSpinner">
+            <IconSpinner />
+        </div>
+    {:then} 
+        <div class="sideNavWrapper">
+            <SideNav/>
+        </div>
+        <div class="contentWrapper">
+            <div class="content">
+                <h2 style="margin-bottom: 1rem; margin-left: 0.5rem;">Hei</h2>
+                <slot></slot>
+            </div>
+        </div>
+    {/await}
+   
+</div>
+
+<style>
+    .sideNavWrapper, .contentWrapper {
+        min-height: 100vh;
+    }
+
+    .sideNavWrapper {
+        position: fixed;
+        width: calc(var(--sidenavWidth));
+        background-color: var(--varme-1);
+        top: 0;
+        bottom: 0;
+        left: 0;
+        overflow-y:auto;
+        overflow-x:hidden;
+    }
+
+    .contentWrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        margin-left: var(--sidenavWidth);
+        width: calc(98vw - var(--sidenavWidth));
+        padding: 0px 32px;
+        overflow:auto;
+    }
+
+    .content {
+        margin-top: 2rem;
+        width: 90%;
+        padding: 3rem;
+    } 
+</style>
