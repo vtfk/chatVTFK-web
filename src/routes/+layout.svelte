9a7f25fc2b9d3df15128b4ea7bb61717b83a9fad
Robin Ellingsen <robin-elli@hotmail.com>
2023-04-18 13:20:45 +0200

    error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,37 +9,37 @@
 
     let msal
     
-    const user = get(userStore)
-    console.log('Jeg kom hit i vercel')
-    if(browser && window.location.pathname === '/') {
-        window.location.href = `${web.url}/authenticated/chatVTFK`
-    }
+    // const user = get(userStore)
+    // console.log('Jeg kom hit i vercel')
+    // if(browser && window.location.pathname === '/') {
+    //     window.location.href = `${web.url}/authenticated/chatVTFK`
+    // }
     // If user is navigating, check if user have a valid token. If the token is not valid, do something
-    $: if($navigating) {
-        try {
-            msal = sessionStorage.getItem(`${user.homeAccountId}-login.windows.net-accesstoken-${user.idTokenClaims.aud}-${user.tenantId}-openid profile user.read email--`)
-            msal = JSON.parse(msal)
-        } catch(error) {
-            console.log(error)
-        } finally { 
-            // If token is valid
-            if(msal !== null && msal.expiresOn > Date.now() / 1000) {
-                goto(`${web.url}/authenticated/chatVTFK`)
-                // if($navigating.to.url.pathname === 'http://localhost:5173/authenticated/chatVTFK') {
-                //     goto('http://localhost:5173/authenticated/chatVTFK')
-                // } else {
-                //     goto($navigating.to.url.pathname)
-                // }
-            } 
-            else {
-                sessionStorage?.clear()
-                userStore.set(undefined)
-                goto($navigating.to.url.pathname)
-                location.reload(true)
-            }
-        }
-    }
-    console.log('Jeg kom hit i vercel2')
+    // $: if($navigating) {
+    //     try {
+    //         msal = sessionStorage.getItem(`${user.homeAccountId}-login.windows.net-accesstoken-${user.idTokenClaims.aud}-${user.tenantId}-openid profile user.read email--`)
+    //         msal = JSON.parse(msal)
+    //     } catch(error) {
+    //         console.log(error)
+    //     } finally { 
+    //         // If token is valid
+    //         if(msal !== null && msal.expiresOn > Date.now() / 1000) {
+    //             goto(`${web.url}/authenticated/chatVTFK`)
+    //             // if($navigating.to.url.pathname === 'http://localhost:5173/authenticated/chatVTFK') {
+    //             //     goto('http://localhost:5173/authenticated/chatVTFK')
+    //             // } else {
+    //             //     goto($navigating.to.url.pathname)
+    //             // }
+    //         } 
+    //         else {
+    //             sessionStorage?.clear()
+    //             userStore.set(undefined)
+    //             goto($navigating.to.url.pathname)
+    //             location.reload(true)
+    //         }
+    //     }
+    // }
+    // console.log('Jeg kom hit i vercel2')
 </script>
 
 <slot></slot>
