204efadd06d64bff1746e71e665df1e755e52ed9
Robin Ellingsen <robin-elli@hotmail.com>
2023-04-18 09:44:21 +0200

    load error

--- src/hooks.client.js
+++ src/hooks.client.js
@@ -1,23 +1,27 @@
-// import login from "./lib/Auth/Login.js"
+import login from "./lib/Auth/Login.js"
 // // import { userStore } from './lib/services/store'
-// import { browser } from "$app/environment"
-// import { varConfig, web } from "../config"
+import { browser } from "$app/environment"
+import { varConfig, web } from "../config"
 
-// let msal = sessionStorage.getItem(varConfig.msal.token)
-// msal = JSON.parse(msal)
-// // Skip login on hard reload if token is still valid
-// if(msal !== null && msal.expiresOn > Date.now() / 1000) {
-//     console.log('Token still valid')
-//     // if(browser) {
-//     //     if(window.location.pathname === '/') {
-//     //         // Nødløsning frem til goto funksjonen fungerer.
-//     //         // window.location.href = `${web.url}/authenticated/chatVTFK`
-//     //         // Dette funker ikke, er noe svelte greier som ikke funker som det skal.
-//     //         // goto('http://localhost:5173/authenticated')
-//     //     }
-//     // }
-   
-// } else {
-//     const user = await login()
-//     // userStore.set(await user)
-// } 
+if(typeof(varConfig) === "object") {
+    let msal = sessionStorage.getItem(varConfig.msal.token)
+    msal = JSON.parse(msal)
+    // Skip login on hard reload if token is still valid
+    if(msal !== null && msal.expiresOn > Date.now() / 1000) {
+        console.log('Token still valid')
+        if(browser) {
+            if(window.location.pathname === '/') {
+                // Nødløsning frem til goto funksjonen fungerer.
+                // window.location.href = `${web.url}/authenticated/chatVTFK`
+                // Dette funker ikke, er noe svelte greier som ikke funker som det skal.
+                // goto('http://localhost:5173/authenticated')
+            }
+        }
+    
+    } else {
+        const user = await login()
+    //     // userStore.set(await user)
+    } 
+} else {
+    await login()
+}
