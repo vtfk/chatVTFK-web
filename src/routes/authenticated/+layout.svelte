1651ed0954fd9222e567f97458c1b5385bbd1e1d
Robin Ellingsen <robin-elli@hotmail.com>
2023-04-18 12:55:48 +0200

    load error

--- src/routes/authenticated/+layout.svelte
+++ src/routes/authenticated/+layout.svelte
@@ -53,7 +53,7 @@
         </div>
         <div class="contentWrapper">
             <div class="content">
-                <h2 style="margin-bottom: 1rem; margin-left: 0.5rem;"> Hei, {msalUser.name} 🤓</h2>
+                <h2 style="margin-bottom: 1rem; margin-left: 0.5rem;"> Hei 🤓</h2>
                 <slot></slot>
             </div>
         </div>
