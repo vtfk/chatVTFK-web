4951f53a6fe925c07948db8944c54d1a52fbafbd
Robin Ellingsen <robin-elli@hotmail.com>
2023-09-14 20:40:37 +0200

    er det mulig? Ja...

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     import { goto } from '$app/navigation'
     import { navigating } from "$app/stores"
     import { browser } from "$app/environment"
-    import { web } from '../../config';
+    import { api, web } from '../../config';
 	import { userStore } from '$lib/services/store';
 	import { get } from 'svelte/store';
 	import login from '$lib/Auth/Login';
@@ -30,7 +30,7 @@
         user = (get(userStore))
         try {
             // msal = sessionStorage.getItem(`${user.homeAccountId}-login.windows.net-accesstoken-${user.idTokenClaims.aud}-${user.tenantId}-openid profile user.read email--`)
-            msal = sessionStorage.getItem(`${user.homeAccountId}-login.windows.net-accesstoken-${user.idTokenClaims.aud}-${user.tenantId}-api://c443279c-2806-488f-b032-c9cf7fa52852/user_impersonation--`)
+            msal = sessionStorage.getItem(`${user.homeAccountId}-login.windows.net-accesstoken-${user.idTokenClaims.aud}-${user.tenantId}-${api.scope}--`)
             if(msal) {
                 msal = JSON.parse(msal)
             } else {
